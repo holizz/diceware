4e1f4348c4783b2a6501631a152d0cfd1cc94a6b
Tom Adams <tom@holizz.com>
2014-05-18 11:12:08 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"crypto/rand"
+	"errors"
+	"flag"
+	"fmt"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	delim := flag.String("d", " ", "delimitor - the thing between the words")
+	count := flag.Int("c", 8, "count - how many words")
+	flag.Parse()
+
+	fmt.Println(diceware(*delim, *count))
+}
+
+func diceware(delim string, count int) string {
+	words := []string{}
+
+	for i := 0; i < count; i++ {
+		words = append(words, word())
+	}
+
+	return strings.Join(words, delim)
+}
+
+func word() string {
+	search := ""
+
+	// Roll 5 dice
+	for i := 0; i < 5; i++ {
+		j, err := rand.Int(rand.Reader, big.NewInt(6))
+		if err != nil {
+			panic(err)
+		}
+
+		search += strconv.Itoa(int(j.Uint64()) + 1)
+	}
+
+	return findWord(search)
+}
+
+func findWord(search string) string {
+	f, err := os.Open("diceware.wordlist.asc")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		t := scanner.Text()
+
+		if strings.HasPrefix(t, search+"\t") {
+			return t[6:]
+		}
+	}
+	panic(errors.New("not found"))
+}
